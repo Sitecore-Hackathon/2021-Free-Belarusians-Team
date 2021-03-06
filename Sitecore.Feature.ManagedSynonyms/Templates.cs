8d6fcde56632e2b28c5a9532bddc4adc9f66bae3
Vitali Taleika <Vitali_Taleika@epam.com>
2021-03-06 08:47:01 +0300

    updated templates

--- Sitecore.Feature.ManagedSynonyms/Templates.cs
+++ Sitecore.Feature.ManagedSynonyms/Templates.cs
@@ -6,12 +6,12 @@ namespace Sitecore.Feature.ManagedSynonyms
     {
         public static class ManagedSynonyms
         {
-            public static readonly ID ManagedSynonymsFolderTemplateId = new ID("{26FD6993-2DDB-468B-B6DC-02241C43B699}");
-            public static readonly ID ManagedSynonymsCoresFieldId = new ID("{D6A8AE8B-572A-485A-9608-39FEE07C48CF}");
+            public static readonly ID ManagedSynonymsFolderTemplateId = new ID("{DF457F0C-B538-40C5-A226-1BCA2857572A}");
+            public static readonly ID ManagedSynonymsCoresFieldId = new ID("{18A793C1-E90C-40EC-8101-6ADACFEC508A}");
 
 
-            public static readonly ID ManagedSynonymTemplateId = new ID("{DF457F0C-B538-40C5-A226-1BCA2857572A}");
-            public static readonly ID SynonymsFieldId = new ID("{DF457F0C-B538-40C5-A226-1BCA2857572A}");
+            public static readonly ID ManagedSynonymTemplateId = new ID("{26FD6993-2DDB-468B-B6DC-02241C43B699}");
+            public static readonly ID SynonymsFieldId = new ID("{D6A8AE8B-572A-485A-9608-39FEE07C48CF}");
         }
     }
 }
